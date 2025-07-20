6a0fd7dc100aae586992ff4bd3b9a07f7e14f7fe
Sadik Rahman <sadik.nai.008@gmail.com>
2025-07-20 20:36:52 +0600

    Important redux notes and redux setup in MegaBlog

--- 12MegaBlog/src/App.jsx
+++ 12MegaBlog/src/App.jsx
@@ -1,12 +1,41 @@
 import './App.css'
+import React, { useEffect, useState } from 'react'
+import authService from "./appwrite/auth.appwrite.js";
+import { useDispatch } from 'react-redux';
+import { login, logout } from './store/authSlice';
+import { Header, Footer } from './components/index.js';
+import { Outlet } from 'react-router-dom';
+
 
 function App() {
-  console.log(import.meta.env.VITE_APPWRITE_URL)
-  return (
-    <>
-      <h1>MEGA PROJECT </h1>
-    </>
-  )
+  
+  const [loading, setLoading] = useState(true);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    authService.getCurrentUser()
+    .then((userData) => {
+      if(userData){
+        dispatch(login(userData))
+      }
+      else{
+        dispatch(logout());
+      }
+    })
+    .finally(() => setLoading(false))
+  }, [])
+  
+  return !loading ? (
+    <div className='min-h-screen flex flex-wrap content-between bg-gray-400'>
+      <div className='w-full block'>
+        <Header />
+        <main>
+          TODO: <Outlet/>
+        </main>
+        <Footer/>
+      </div>  
+    </div>
+  ) : null
 }
 
 export default App
