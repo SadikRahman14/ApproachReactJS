e7bec86277320e6073aa4192f2d48724a907d1e0
Sadik Rahman <sadik.nai.008@gmail.com>
2025-07-26 11:14:50 +0600

    Implement routing and page components for authentication and post management

--- 12MegaBlog/src/main.jsx
+++ 12MegaBlog/src/main.jsx
@@ -6,7 +6,77 @@ import { Provider } from 'react-redux'
 import store from './store/store.js'
 import ReactDOM from 'react-dom/client';
 import React from 'react'
+import { createBrowserRouter } from 'react-router-dom'
+import { AuthLayout } from './components/index.js'
 
+import Signup from './pages/Signup.jsx'
+import AddPost from './pages/AddPost.jsx'
+import AllPosts from './pages/AllPosts.jsx'
+import Post from './pages/Post.jsx'
+import EditPost from './pages/EditPost.jsx'
+
+
+const router = createBrowserRouter([
+  {
+    path : "/",
+    element: <Home/>,
+    children: [
+      {
+        path : "/",
+        element : <Home/>
+      },
+      {
+        path : "/login",
+        element : (
+          <AuthLayout authentication = {false}>
+            <Login />
+          </AuthLayout>
+        )
+      },
+      {
+        path : "/signup",
+        element : (
+          <AuthLayout authentication = {false}>
+            <Signup />
+          </AuthLayout>
+        )
+      },
+      
+      {
+        path : "/all-post",
+        element : (
+          <AuthLayout authentication>
+            {" "}
+            <AllPosts/>            
+          </AuthLayout>
+        )
+      },
+      {
+        path : "/add-post",
+        element : (
+          <AuthLayout authentication>
+            <AddPost />
+          </AuthLayout>
+        )
+      },
+      {
+          path: "/edit-post/:slug",
+          element: (
+              <AuthLayout authentication>
+                  {" "}
+                  <EditPost />
+              </AuthLayout>
+          ),
+      },
+
+      {
+          path: "/post/:slug",
+          element: <Post />,
+      },
+    ]
+
+  }
+])
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
